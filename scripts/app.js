ddd0c2bcf7cd9e7ea5e143e9bfe70f34e5c13683
umairayub-dev <umairayub049@gmail.com>
2023-04-09 12:26:20 +0500

    DAY 01

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+// DAY 01 (9 APRIL)
+
+// alert("Hello")
+// document.write("<h1>hello</h1>")
+// console.log("hello")
+// prompt("Enter a number between 1 - 10")
+
+// cases
+// var namePrompt //camelCase
+// var NamePrompt //PascalCase
+// var name_prompt //snake_case
+
+// var namePrompt = prompt("Enter your name:")
+// var greet = "How are you?"
+// alert("Hello " namePrompt + " " + greet)
+
+// var num = "3"
+// console.log(typeof num)
+
+// var myAge = +prompt("Your Age")
+// var FathersAge = +prompt("Your Fathers Age")
+
+// console.log(myAge + FathersAge)
+
+// var num = 1
+// console.log(num)
+// num = num + 1
+// num+=1
+
+// DAY 02 (10 APRIL)
